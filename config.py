b72c8ff16eaa6d60215969e4b387b93d15e875a7
Sefstel <sefstelarm@gmail.com>
2021-12-21 16:35:48 +0300

    Закрытие дня

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-newer-guess'
-    
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
+        'sqllite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
