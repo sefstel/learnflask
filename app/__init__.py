b689a985c7fb33cb872d0a7202f685af709f4126
Sefstel <sefstelarm@gmail.com>
2021-12-26 17:17:47 +0300

    Добавлены сессии

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+login = LoginManager(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db,render_as_batch=True)
 
 from app import  models,routes
+
+login = LoginManager(app)
+login.login_view = 'login'
