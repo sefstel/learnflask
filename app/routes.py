a99570f5c5d1a53b50c493f56282aa2134db4e8c
Sefstel <sefstelarm@gmail.com>
2022-01-02 19:52:49 +0300

    Add registration page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm, BlogForm
+from app.forms import LoginForm, BlogForm, RegistrationForm
 from app import db
 from app.models import Blog
 from flask_login import current_user, login_user, logout_user, login_required
@@ -71,3 +71,18 @@ def delete_blog(id):
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
